3d07bb92e45776e25c740e67c400cbca0d22d92d
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-07-18 20:01:35 +0300

    доработка апскедулар

--- main/management/commands/runapscheduler.py
+++ main/management/commands/runapscheduler.py
@@ -1,18 +1,18 @@
-from django.conf import settings
-
-from apscheduler.schedulers.background import BackgroundScheduler
 # from apscheduler.schedulers.blocking import BlockingScheduler
 # BlockingScheduler Блокирует терминал, в отличии от BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
-from django.core.management.base import BaseCommand
-from apscheduler.schedulers.jobstores import DjangoJobStore
-from apscheduler.schedulers.models import DjangoJobExecution
-from apscheduler.schedulers import util
-from main.services import send_all_mails
 # завершить ctrl+C
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+from django_apscheduler.util import close_old_connections
+
+from main.services.sending import send_all_mails
 
 
-@util.close_old_connections
+@close_old_connections
 def delete_old_job_executions(max_age=604_800):
     DjangoJobExecution.objects.delete_old_job_executions(max_age)
 
