d78ba4da129395d577749c1d0b029c3ca2763e95
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-07-23 18:23:05 +0300

    Добавление новых форм

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import BooleanField, BaseInlineFormSet
-from main.models import Settings
+from main.models import Settings, Client, Message
 
 
 class StyleFormMixin:
@@ -14,7 +14,44 @@ class StyleFormMixin:
 
 
 class SettingsForm(StyleFormMixin, forms.ModelForm):
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields['clients'].queryset = Client.objects.filter(owner=user)
+        # self.fields['message'].queryset = Message.objects.filter(owner=user)
 
     class Meta:
         model = Settings
+        exclude = ('owner',)
+
+
+class SettingsManagerForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Settings
+        fields = ('message', 'clients',)
+
+
+class ClientForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Client
+        exclude = ('is_banned', 'owner',)
+
+
+class ClientManagerForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+
+class MessageForm(StyleFormMixin, forms.ModelForm):
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = Message
+        exclude = ('owner',)
+
+
+class MessageManagerForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Message
         fields = '__all__'
